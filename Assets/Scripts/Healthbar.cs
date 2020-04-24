7fb835b69bebb61a2f008916b0b18762ee09ab51
Wolfgang Guan <wolfgang.guan56@gmail.com>
2020-04-24 16:08:31 -0400

    added prefab for health bar and sample mob

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour {
+
+    public Image healthImage;
+    public int maxHealth;
+
+    void Start () {
+    }
+
+    void Update () {
+    }
+
+    /**
+     * Sets the number of health points left, with valid range from 0 to maxHealth.
+     */
+    public void SetHealth(int newHealth) {
+        healthImage.fillAmount = (float) newHealth / (float) maxHealth;
+    }
+
+    /**
+     * Sets the health value left in percentage, with valid range from 0.0 to 1.0;
+     */
+    public void SetHealthPct(float newHealth) {
+        healthImage.fillAmount = newHealth;
+    }
+}
