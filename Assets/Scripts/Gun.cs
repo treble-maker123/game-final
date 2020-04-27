69c1ca47de1eeaa9afde6c15ed57b5749db89f0e
WilliamGoss <WilliamTGoss@gmail.com>
2020-04-27 15:34:28 -0400

    FPS Initial Implementation
    Cluttering the repo with all the Standard Assets and some textures.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public ParticleSystem muzzleFlash;
+
+    public Camera fpsCamera;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hitInfo;
+        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.name);
+        }
+    }
+}
