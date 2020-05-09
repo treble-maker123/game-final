41db85664c2ad86098e901de132164d22e43b710
WilliamGoss <WilliamTGoss@gmail.com>
2020-05-09 04:10:18 -0400

    Tutorial Implemented
    Tutorial script contains all the logic for the tutorial, fully
    implemented.
    
    The tutorial is partially completed, just three more chunks to cover.

---
+++ Assets/Tutorial.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+
+    public Button weapon;
+    public Button weaponBack;
+    public Button towerNext;
+    public Button generalNext;
+    public Button towerBack;
+    public Button buildNext;
+    public Button buildBack;
+    public Button upgradeNext;
+    public Button upgradeBack;
+    public Button mobNext;
+    public Button mobBack;
+    public Button mainMenu;
+
+    public GameObject startUI;
+    public GameObject weaponUI;
+    public GameObject towerUI;
+    public GameObject generalUI;
+    public GameObject buildUI;
+    public GameObject upgradeUI;
+    public GameObject mobUI;
+
+	public void Weapon()
+    {
+        startUI.SetActive(false);
+        weaponUI.SetActive(true);
+    }
+
+    public void WeaponBack()
+    {
+        towerUI.SetActive(false);
+        weaponUI.SetActive(true);
+    }
+
+    public void Towers()
+    {
+        weaponUI.SetActive(false);
+        towerUI.SetActive(true);
+    }
+
+    public void TowerBack()
+    {
+        generalUI.SetActive(false);
+        towerUI.SetActive(true);
+    }
+
+    public void General()
+    {
+        towerUI.SetActive(false);
+        generalUI.SetActive(true);
+    }
+
+    public void GeneralBack()
+    {
+        buildUI.SetActive(false);
+        generalUI.SetActive(true);
+    }
+
+    public void Build()
+    {
+        generalUI.SetActive(false);
+        buildUI.SetActive(true);
+    }
+
+    public void BuildBack()
+    {
+        upgradeUI.SetActive(false);
+        buildUI.SetActive(true);
+    }
+
+    public void Upgrade()
+    {
+        buildUI.SetActive(false);
+        upgradeUI.SetActive(true);
+    }
+
+    public void UpgradeBack()
+    {
+        mobUI.SetActive(false);
+        upgradeUI.SetActive(true);
+    }
+
+    public void Mobs()
+    {
+        upgradeUI.SetActive(false);
+        mobUI.SetActive(true);
+    }
+
+    public void Menu()
+    {
+        //Get Main Menu Scene
+        Debug.Log("Back to menu");
+    }
+}
