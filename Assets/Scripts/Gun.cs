500a64584bf7fcbf55e3641e4d54566e974ae844
WilliamGoss <WilliamTGoss@gmail.com>
2020-05-09 01:15:06 -0400

    Rifle firing rate adjusted
    Much more reasonable now.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Gun : MonoBehaviour {
+public class Gun : MonoBehaviour
+{
 
     public float damage = 10f;
     public float range = 100f;
     public ParticleSystem muzzleFlash;
+    public float fireRate = 0.01f;
+    private float nextShot = 0.0f;
 
     public Camera fpsCamera;
 
     // Update is called once per frame
-    void Update () {
-        if(Input.GetMouseButton(0))
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time > nextShot)
         {
+            nextShot = Time.time + fireRate;
             Shoot();
         }
     }
@@ -24,13 +29,13 @@ public class Gun : MonoBehaviour {
         RaycastHit hitInfo;
 
         Vector3 adj = new Vector3(Random.RandomRange(-0.8f, 0.8f), Random.RandomRange(-0.8f, 0.8f));
-        if(Physics.Raycast(fpsCamera.transform.position + adj, fpsCamera.transform.forward, out hitInfo, range))
+        if (Physics.Raycast(fpsCamera.transform.position + adj, fpsCamera.transform.forward, out hitInfo, range))
         {
             var e = Resources.Load("HitSpot");
             Instantiate(e, hitInfo.point, Quaternion.identity);
             Debug.Log(hitInfo.transform.name);
             //This is where we'll check if it's a hittable game object that can take damage.
-            if(hitInfo.transform.tag == "Mobs")
+            if (hitInfo.transform.tag == "Mobs")
             {
                 MobInteraction mob = hitInfo.transform.GetComponent<MobInteraction>();
                 mob.TakeDamage(10f);
